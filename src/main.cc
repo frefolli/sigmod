b0794f1d0a4c5e506c6ff689257fac52bfe57749
Telemaco Terzi <tezze.terzi01@gmail.com>
2024-03-29 13:28:05 +0100

    added ETA and added test exe in the gitignore

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,6 @@
+#include <chrono>
 #include <sigmod.hh>
+#include <iostream>
 
 int main(int argc, char** args) {
     std::string database_path = "dummy-data.bin";
@@ -16,6 +18,15 @@ int main(int argc, char** args) {
     if (argc > 3) {
         output_path = std::string(args[3]);
     }
-    
+
+    auto start = std::chrono::high_resolution_clock::now();
+
     Workflow(database_path, query_set_path, output_path);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout 
+        << "ETA (ms): " 
+        << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()  
+        << std::endl;
 }
