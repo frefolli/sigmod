5dfd6f2e933e4af43fa1b394ad0027503a7800d8
Refolli Francesco <f.refolli@campus.unimib.it>
2024-04-12 10:59:40 +0200

    ulteriori prove

--- prova.py
+++ prova.py
@@ -7,24 +7,24 @@ double const y = __builtin_bit_cast(double, MAGIC_NUMBER_A - (__builtin_bit_cast
 return 1 / (y * C * (D - number * y * y));
 
 #define MAGIC_NUMBER_A 0x5FE6EB50C7B537A9
+#define MAGIC_NUMBER_B 0x5F1FFFF9
 #define C 0.703952253f
 #define D 2.38924456f
 """
 
-def double_to_ulong(double):
-    return struct.unpack('@Q', struct.pack('@d', f))[0]
-
-def ulong_to_double(double):
-    return struct.unpack('@d', struct.pack('@Q', f))[0]
-
 def quacke3(x, C, D):
-    y = ulong_to_double(0x5FE6EB50C7B537A9 - (double_to_ulong(x) // 2))
+    print(x.shape)
+    y = (-(x.view(np.uint32) / 2) + 0x5F1FFFF9).view(np.float32)
     return 1 / (y * C * (D - x * y * y))
 
 # Generate some sample data
-x_data = ["x"]
-y_data = ["sqrt(x)"]
+x_data = list(np.linspace(0, 300, num=200000, dtype=np.float32))
+y_data = np.sqrt(x_data)
+
+C0 = 0.703952253
+D0 = 2.38924456
 
 # Fit the data to the logarithm function
-popt, pcov = curve_fit(quacke3, x_data, y_data)
+popt, pcov = curve_fit(quacke3, x_data, y_data, p0=(C0, D0))
+print(y_data[434], quacke3(x_data[434], *popt))
 print(popt)
