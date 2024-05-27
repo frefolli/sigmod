3719f3f37b059c3ff574b77cdf007133745d983c
Telemaco Terzi <tezze.terzi01@gmail.com>
2024-05-27 15:56:40 +0200

    changes

--- grid_search.py
+++ grid_search.py
@@ -64,8 +64,8 @@ def grid_search():
             #'LSH_TABLES': "%s"  % LSH_TABLES[dx],
             #'LSH_WIDTH(width)': "%s" % 
         })
-        out = 'output-1m-lsh-forest-treshold-%s.txt' % LSH_FOREST_TRESHOLD[dx]
-        execute_script('contest-1m', out)
+        out = 'output-10m-lsh-forest-treshold-%s.txt' % LSH_FOREST_TRESHOLD[dx]
+        execute_script('contest-10m', out)
         inc = extract_data(out)
         inc['LSH_FOREST_TRESHOLD'] = LSH_FOREST_TRESHOLD[dx]
         cum = aggregate(cum, inc)
